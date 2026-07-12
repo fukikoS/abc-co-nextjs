101083a0fc87f4f5981ea0edf6e44b26916d7e15
Fukiko Sakoda <fukiko.sakoda@gmail.com>
2026-07-12 21:47:19 +0900

    fix: SITE_URLのデフォルト値を本番URLに変更する

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 const SITE_NAME = "ABC Co.";
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://abc-co-next.fscodelab.com/";
 
 export { SITE_NAME, SITE_URL };
